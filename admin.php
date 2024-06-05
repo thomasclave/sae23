e0d35e818cf4cbe3091237dec8904ca5d2936169
Thomas Clavé <66850245+thomasclave@users.noreply.github.com>
2024-06-05 17:38:39 +0200

    Structure des fichiers PHP/HTML

---
+++ admin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAE23</title>
+
+    <!--CSS links
+    <link rel="stylesheet" href="/css/style.css">
+    -->
+    <!--Policies links
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    -->
+</head>
+<body>
+    <!--Navbar-->
+    <nav class="navbar">
+        <a class="nav-nom" href="index.html">SAE23</a>
+        <ul class="nav-links">
+            <li><a href="./index.php">Accueil</a></li>
+            <li><a href="./consultation.php">Consultation</a></li>
+            <li><a href="./gestion.php">Gestion</a></li>
+            <li><a href="./gestion_projet.html">Gestion de Projet</a></li>
+            <li><a href="./admin.php">Administration</a></li>
+        </ul>
+     </nav>
+     
+     <!--Title-->
+     <header>
+     <h2>Page d'administration</h2>
+     </header>
+
+<main>
+
+
+</main>
+
+<!--Footer-->
+<footer>
+    Site réalisé dans le cadre de la SAE23<br>
+</footer>
+
+</body>
+</html>
