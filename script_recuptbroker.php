290f677b61eb99a61f899f511ad3b512eaaaa506
Fl0riann <flooo.meissonnier@gmail.com>
2024-06-11 09:36:00 +0200

    Traduction

--- script_recuptbroker.php
+++ script_recuptbroker.php
@@ -20,7 +20,7 @@ while (true) {
     $measurements = $decodedData[0];
     $deviceInfo = $decodedData[1];
 
-    // Informations de l'appareil
+    // recover the room and the building
     $roomName = $deviceInfo['room'];
     $buildingId = $deviceInfo['Building'];
 
@@ -39,7 +39,7 @@ while (true) {
         }
     }
 
-    // Vérifier et insérer la salle
+    // Check and insert room
     $checkRoomQuery = "SELECT * FROM salle WHERE NomSalle = '$roomName'";
     $roomResult = mysqli_query($databaseConnection, $checkRoomQuery);
 
@@ -54,14 +54,14 @@ while (true) {
         }
     }
 
-    // Insérer les capteurs et les mesures
+    //look in the data table for all the sensor types and retrieves the value
     foreach ($measurements as $sensorType => $sensorValue) {
-        // Ignorer les capteurs indésirables
+        // Ignore unwanted sensors
         if (in_array($sensorType, ['activity', 'infrared', 'tvoc', 'infrared_and_visible'])) {
             continue;
         }
 
-        // Déterminer l'unité du capteur (exemple simplifié)
+        //Determine the sensor unit
         $unit = '';
         switch ($sensorType) {
             case 'temperature':
@@ -83,11 +83,13 @@ while (true) {
                 $unit = '';
         }
 
-        // Construire le nom du capteur en utilisant le nom de la salle suivi du type de capteur
+        // Construct the sensor name using the room name followed by the sensor type
         $sensorName = $roomName . '_' . $sensorType;
+        // Check if the sensor already exists in the database
         $checkSensorQuery = "SELECT * FROM capteur WHERE NomCapt = '$sensorName'";
         $sensorResult = mysqli_query($databaseConnection, $checkSensorQuery);
 
+        // If the sensor does not exist, insert it into the database
         if (mysqli_num_rows($sensorResult) == 0) {
             $insertSensorQuery = "INSERT INTO capteur (NomCapt, TypeCapt, Unite, NomSalle) VALUES ('$sensorName', '$sensorType', '$unit', '$roomName')";
             if (!mysqli_query($databaseConnection, $insertSensorQuery)) {
@@ -97,7 +99,7 @@ while (true) {
             }
         }
 
-        // Insérer la mesure
+        // Insert date and time into database
         $currentDate = date('Y-m-d');
         $currentTime = date('H:i:s');
         $insertMeasurementQuery = "INSERT INTO mesure (Date, Horaire, Valeur, NomCapt) VALUES ('$currentDate', '$currentTime', '$sensorValue', '$sensorName')";
@@ -109,6 +111,4 @@ while (true) {
         }
     }
 }
-
-// La boucle continue indéfiniment, donc la connexion ne sera jamais fermée
 ?>
